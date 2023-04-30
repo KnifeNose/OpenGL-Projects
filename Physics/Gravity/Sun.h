aa970c84c7674ed81755e38bc903950365217f11
KnifeNose <126018005+KnifeNose@users.noreply.github.com>
2023-04-30 06:25:39 +0100

    Create Sun.h

---
+++ Physics/Gravity/Sun.h
@@ -0,0 +1,16 @@
+#ifndef SUN_H
+#define SUN_H
+
+class Sun
+{
+public:
+	void drawSun(float circleX, float circleY, float circleRadius, int numOfSeg);
+
+	float circleX = 0;
+	float circleY = 0;
+	float sunMass = 1;
+
+};
+
+
+#endif // !SUN_H
