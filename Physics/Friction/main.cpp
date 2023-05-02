1cc1451e011843b884512ae48b868bd50c5de620
KnifeNose <126018005+KnifeNose@users.noreply.github.com>
2023-05-02 07:08:04 +0100

    Create main.cpp

---
+++ Physics/Friction/main.cpp
@@ -0,0 +1,60 @@
+#include <GLFW/glfw3.h>
+#include "Ground.h"
+#include "Box.h"
+
+int main(void)
+{
+    GLFWwindow* window;
+
+    /* Initialize the library */
+    if (!glfwInit())
+        return -1;
+
+    /* Create a windowed mode window and its OpenGL context */
+    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    if (!window)
+    {
+        glfwTerminate();
+        return -1;
+    }
+
+    /* Make the window's context current */
+    glfwMakeContextCurrent(window);
+
+    Ground grounds[5];
+    Box b;
+
+
+    /* Loop until the user closes the window */
+    while (!glfwWindowShouldClose(window))
+    {
+        /* Render here */
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        float x = -1;
+    for (int i = 0; i < 5; i++)
+    {
+        float temp = 0.4f;
+        float y = -1;
+
+        grounds[i].drawGround(x, y, 0.5f, 0.5f);
+        x += temp;
+    }
+
+        glColor3f(0.0f, 1.0f, 0.0f);
+
+        b.drawBox(b.posX, b.posY, 0.2f, 0.1f);
+        b.motion();
+        b.addForce(0.0000001f);
+        b.addFriction();
+
+        /* Swap front and back buffers */
+        glfwSwapBuffers(window);
+
+        /* Poll for and process events */
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return 0;
+}
