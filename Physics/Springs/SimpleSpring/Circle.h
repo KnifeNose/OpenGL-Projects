3f38404334cfac147cbd85cbf34ee014030391c3
KnifeNose <126018005+KnifeNose@users.noreply.github.com>
2023-05-06 09:29:02 +0100

    Create Circle.h

---
+++ Physics/Springs/SimpleSpring/Circle.h
@@ -0,0 +1,17 @@
+#ifndef CIRCLE_H
+#define CIRCLE_H
+
+class Circle
+{
+public:
+
+	void drawCircle(float centerX, float centerY, float radius, int numOfSeg);
+
+	void drawLine(float x1, float y1, float x2, float y2);
+};
+
+
+
+
+
+#endif // !CIRCLE_H
