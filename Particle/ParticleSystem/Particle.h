27408437357903745796d02741cd7dc75b599acd
KnifeNose <126018005+KnifeNose@users.noreply.github.com>
2023-05-14 08:39:36 +0100

    Create Particle.h

---
+++ Particle/ParticleSystem/Particle.h
@@ -0,0 +1,35 @@
+#ifndef PARTICLE_H
+#define PARTICLE_H
+class Particle
+{
+public:
+	Particle();
+
+	void drawCircle(float centerX, float centerY, float radius, int numOfSeg);
+
+	void motion();
+
+	void addForce(float force[2]);
+
+	void startFunc(float x, float y);
+
+	void color();
+
+	void enableGravity();
+
+	float position[2];
+
+	bool isFirstFrame;
+
+	float health;
+
+private:
+
+	float velocity[2];
+	float acceleration[2];
+
+	float mass;
+	float damping;
+};
+
+#endif
